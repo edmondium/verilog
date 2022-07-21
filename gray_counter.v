a620c97a34eaa460337b64afb71c6656971e0ef6
edmondium <nicko@uin-suska.ac.id>
2022-07-21 18:17:11 +0700

    example of documentation

--- gray_counter.v
+++ gray_counter.v
@@ -1,12 +1,11 @@
-//! Module description
-//
+//! Module description 
+// documentation
 //! ***this is code*** sample
 //! | Tables   | Are           | Cool  |
 //! |----------|:-------------:|------:|
 //! | col 1 is | left-aligned  | $1600 |
 //! | col 2 is | centered      | $12   |
 //! | col 3 is | right-aligned | $1    |
-
 module gray_counter(
     out   , // counter out
     clk   , //! clock
@@ -15,10 +14,8 @@ module gray_counter(
     clk3  , //! clock comment
     rst     //! **active high reset**
 );
-
     input clk, clk1, clk2, clk3, rst;
     output [7:0] out;
     wire [7:0] out;
     reg [7:0] count;
-    
 endmodule
