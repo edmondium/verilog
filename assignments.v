fa128bfeedded0c8ddf620a58ecc280f73bab43d
edmondium <nicko@uin-suska.ac.id>
2022-07-29 18:49:43 +0700

    non-blocking program

---
+++ assignments.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: LinkedIn Learning
+// Engineer: Eduardo Corpeño
+// 
+// Create Date: 11/04/2019 09:56:07 PM
+// Design Name: 
+// Module Name: assignments
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+module assignments (
+    input i,
+    input clk,
+    output reg o
+);
+    reg temp;
+    always @ (posedge clk) begin
+        temp <= i;
+        o <= temp;
+    end
+endmodule
